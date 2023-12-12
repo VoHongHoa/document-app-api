a9f5e64ed4c9a67af9a0df70c8e869ceb5ac6381
VoHongHoa <19521522@gm.uit.edu.vn>
2023-12-12 08:48:05 +0700

    u

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ConfigService } from '@nestjs/config';
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.use(cors());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
