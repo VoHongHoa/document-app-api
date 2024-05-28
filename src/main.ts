5336a972efe7df9de4eecfd3f7f75abe0bd3fe86
hoavh.qni <hoavh.qni@vnpt.vn>
2024-05-28 15:55:11 +0700

    u

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ConfigService } from '@nestjs/config';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalFilters(new GlobalExceptionFilter());
-  app.use(cors());
+  // app.use(cors({
+  //   origin: configService.get('REACT_APP_URL'),
+  // }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
